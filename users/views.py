dd0ef86374b470f7df150d1aa39e5771b939ff75
Hirdeshgupta <hirdeshgupta16@gmail.com>
2021-09-21 23:29:31 +0530

    Users Authentication login logout

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from typing import Pattern
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def Register(request):
@@ -10,9 +11,15 @@ def Register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(
+                request, f'Your Account is Successfully created please log in !')
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 # Create your views here.
