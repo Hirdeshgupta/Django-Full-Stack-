310049cd7b4268b3ee6e996119e54579d8a1d881
Hirdeshgupta <hirdeshgupta16@gmail.com>
2021-09-22 12:50:15 +0530

    authentication and profile completed

--- blog/views.py
+++ blog/views.py
@@ -10,4 +10,5 @@ def home(request):
 def about(request):
     return render(request, 'blog/about.html')
 
+
 # Create your views here.
--- users/views.py
+++ users/views.py
@@ -1,13 +1,13 @@
 from typing import Pattern
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .form import UserRegisterForm, UserUpdateForm, UserProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 
 def Register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -15,11 +15,23 @@ def Register(request):
                 request, f'Your Account is Successfully created please log in !')
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = UserProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Sucessfullly Updated Profile !')
+            redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = UserProfileUpdateForm(instance=request.user.profile)
+    return render(request, 'users/profile.html', {'u_form': u_form, 'p_form': p_form})
 # Create your views here.
